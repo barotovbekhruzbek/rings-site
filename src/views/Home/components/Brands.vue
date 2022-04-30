b4445afb33a5b71ec5aa4f93ed217f86dda4cbba
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2022-05-01 01:35:35 +0300

    changed all file

--- src/views/Home/components/Brands.vue
+++ src/views/Home/components/Brands.vue
@@ -2,7 +2,7 @@
   <div class="brands">
       <div class="container">
           <div class="brand" v-for="brand in brands" :key="brand.img">
-              <img :src="brand.img" alt="">
+              <img :src="brand.img" alt="logo">
           </div>
       </div>
     </div>
@@ -27,6 +27,9 @@ export default {
                 },
                   {
                     img:require('../../../assets/images/logos/partner.png')
+                },
+                   {
+                    img:require('../../../assets/images/logos/partner.png')
                 }
             ]
         }
@@ -35,18 +38,29 @@ export default {
 </script>
 
 <style lang="scss">
-     @import '../assets/styles/components/_vars.scss';
-    @import '../assets/styles/components/_mixins.scss';
+     @import '../../../assets/styles/components/_vars.scss';
+    @import '../../../assets/styles/components/_mixins.scss';
     .brands{
+        width: 100%;
      padding: 30px 0;
       border-top: 1px solid $white;  
       .container{
           display: flex;
+        //  Media
+        @include md{
+            flex-wrap: wrap;
+            justify-content: center;
+            gap:10px 0;
+        }
 
           .brand{
              width: 190px; 
              padding: 0 15px;
              border-right: 1px solid $white;
+             @include sm{
+                 width: 30%;
+                 border: none;
+             }
 
              &:last-child{
                  border: none;
@@ -54,7 +68,7 @@ export default {
              }
 
              img{
-
+                width: 100%;
              }
           }
       }
